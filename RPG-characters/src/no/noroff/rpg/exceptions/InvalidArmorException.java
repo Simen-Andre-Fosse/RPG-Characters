1daaf5740e4c7366e3f915005a6e918e22fa2f29
Simen Andre Gravdal <simen.gravdal@hotmail.com>
2022-02-20 23:37:31 +0100

    Exceptions.. comments

--- RPG-characters/src/no/noroff/rpg/exceptions/InvalidArmorException.java
+++ RPG-characters/src/no/noroff/rpg/exceptions/InvalidArmorException.java
@@ -1,5 +1,9 @@
 package no.noroff.rpg.exceptions;
 
+
+/**
+ * Exception class for taking care of exceptions related to Armor.
+ */
 public class InvalidArmorException extends Exception{
     public InvalidArmorException(String errorMessage){
         super(errorMessage);
--- RPG-characters/src/no/noroff/rpg/exceptions/InvalidWeaponException.java
+++ RPG-characters/src/no/noroff/rpg/exceptions/InvalidWeaponException.java
@@ -1,5 +1,9 @@
 package no.noroff.rpg.exceptions;
 
+
+/**
+ * Exception class for taking care of exceptions related to Weapon
+ */
 public class InvalidWeaponException extends Exception {
     public InvalidWeaponException(String errorMessage){
         super(errorMessage);
