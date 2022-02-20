174627bdc8c1923931e59756abd3d7680af5d443
Simen Andre Gravdal <simen.gravdal@hotmail.com>
2022-02-20 23:25:49 +0100

    Update Main.java

--- RPG-characters/src/no/noroff/rpg/Main.java
+++ RPG-characters/src/no/noroff/rpg/Main.java
@@ -8,30 +8,15 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
         /*
-        Weapon sword = new Weapon("Katana", 2, "weapon");
-        for (Weapon.Type myVar : Weapon.Type.values()){
-            if(Objects.equals(myVar.toString(), "SWORD")){
-                System.out.println(myVar);
-            }
-        }
-
+            Example of a character being made and interacted with, then have its stats displayed.
          */
-
         Mage Gandalf = new Mage("Gandalf");
-        System.out.println(Gandalf.getDPS());
-       // System.out.println(Gandalf.getLevel() + " ," + Gandalf.getBaseAtt().getIntelligence());
         Gandalf.levelUp();
-       // System.out.println(Gandalf.getLevel() + " ," + Gandalf.getBaseAtt().getIntelligence());
         Weapon staff = new Weapon("Staff",1, SlotSpot.WEAPON, WeaponType.STAFF);
         Gandalf.equip(staff);
-        //Gandalf.printStats();
-        System.out.println(CharacterStatsDisplay.statsDisplay(Gandalf));
-        System.out.println();
-        System.out.println();
-
         Gandalf.levelUp();
-
         Armor robe = new Armor("Robe", 2, SlotSpot.BODY, ArmorType.CLOTH);
         Gandalf.equip(robe);
         System.out.println(Gandalf.printStats());
